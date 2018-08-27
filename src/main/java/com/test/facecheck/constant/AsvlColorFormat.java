ec69f14b8025b0cb921f45880c00bcec67e2e28f
a324670547 <324670547@qq.com>
2018-08-27 10:58:02 +0800

    视频格式定义

---
+++ src/main/java/com/test/facecheck/constant/AsvlColorFormat.java
@@ -0,0 +1,11 @@
+package com.test.facecheck.constant;
+/**
+ * 视频格式定义
+ */
+public class AsvlColorFormat {
+    public static final int ASVL_PAF_I420 = 0x601;
+    public static final int ASVL_PAF_NV12 = 0x801;
+    public static final int ASVL_PAF_NV21 = 0x802;
+    public static final int ASVL_PAF_YUYV = 0x501;
+    public static final int ASVL_PAF_RGB24_B8G8R8 = 0x201;
+}
