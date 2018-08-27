7cfd4ff0b75f39d8cfec2fd0bf69586ac63bfbe8
a324670547 <324670547@qq.com>
2018-08-27 10:54:06 +0800

    矩阵实体

---
+++ src/main/java/com/test/facecheck/entity/MRect.java
@@ -0,0 +1,49 @@
+package com.test.facecheck.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.sun.jna.Pointer;
+import com.sun.jna.Structure;
+/**
+ * 用于存储人脸坐标的实体类
+ */
+public class MRect extends Structure {
+
+	public int left;
+	public int top;
+	public int right;
+	public int bottom;
+
+	public MRect() {}
+
+	public MRect(Pointer p) {
+		super(p);
+		read();
+	}
+
+	public static class ByValue extends MRect implements Structure.ByValue {
+		public ByValue() {}
+		public ByValue(Pointer p) {
+			super(p);
+		}
+	}
+
+	public static class ByReference extends MRect implements Structure.ByReference {
+		public ByReference() {}
+		public ByReference(Pointer p) {
+			super(p);
+		}
+	};
+
+	@Override
+	protected List<String> getFieldOrder() {
+		List<String> list = new ArrayList<String>();
+		list.add("left");
+		list.add("top");
+		list.add("right");
+		list.add("bottom");
+		return list;
+	}
+
+}
