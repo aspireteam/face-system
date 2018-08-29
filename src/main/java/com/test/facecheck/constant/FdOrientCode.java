2cf33da86491703ff69580fc6742ad510dec6d5d
a324670547 <324670547@qq.com>
2018-08-29 17:53:07 +0800

    定义人脸检测结果中的人脸角度

--- src/main/java/com/test/facecheck/constant/FdOrientCode.java
+++ src/main/java/com/test/facecheck/constant/FdOrientCode.java
@@ -3,16 +3,28 @@ package com.test.facecheck.constant;
  * 定义人脸检测结果中的人脸角度
  */
 public class FdOrientCode {
-    public static final int AFD_FSDK_FOC_0 = 0x1;// 0 degree
-    public static final int AFD_FSDK_FOC_90 = 0x2; // 90 degree
-    public static final int AFD_FSDK_FOC_270 = 0x3; // 270 degree
-    public static final int AFD_FSDK_FOC_180 = 0x4; // 180 degree
-    public static final int AFD_FSDK_FOC_30 = 0x5; // 30 degree
-    public static final int AFD_FSDK_FOC_60 = 0x6; // 60 degree
-    public static final int AFD_FSDK_FOC_120 = 0x7; // 120 degree
-    public static final int AFD_FSDK_FOC_150 = 0x8; // 150 degree
-    public static final int AFD_FSDK_FOC_210 = 0x9; // 210 degree
-    public static final int AFD_FSDK_FOC_240 = 0xa; // 240 degree
-    public static final int AFD_FSDK_FOC_300 = 0xb; // 300 degree
-    public static final int AFD_FSDK_FOC_330 = 0xc; // 330 degree
+	// 0°
+    public static final int AFD_FSDK_FOC_0 = 0x1;
+    // 90°
+    public static final int AFD_FSDK_FOC_90 = 0x2; 
+    // 270°
+    public static final int AFD_FSDK_FOC_270 = 0x3;
+    // 180°
+    public static final int AFD_FSDK_FOC_180 = 0x4;
+    // 30°
+    public static final int AFD_FSDK_FOC_30 = 0x5; 
+    // 60°
+    public static final int AFD_FSDK_FOC_60 = 0x6; 
+    // 120°
+    public static final int AFD_FSDK_FOC_120 = 0x7; 
+    // 150°
+    public static final int AFD_FSDK_FOC_150 = 0x8; 
+    // 210°
+    public static final int AFD_FSDK_FOC_210 = 0x9;
+    // 240°
+    public static final int AFD_FSDK_FOC_240 = 0xa; 
+    // 300°
+    public static final int AFD_FSDK_FOC_300 = 0xb; 
+    // 330°
+    public static final int AFD_FSDK_FOC_330 = 0xc;
 }
