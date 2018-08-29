70259c4fea623c1c9b2800430ccb7eb9685b8446
a324670547 <324670547@qq.com>
2018-08-29 18:11:18 +0800

    存储版本信息实体

---
+++ src/main/java/com/test/facetrack/entity/SdkVersion.java
@@ -0,0 +1,87 @@
+package com.test.facetrack.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.sun.jna.Structure;
+/**
+ * 存储版本信息实体
+ */
+public class SdkVersion extends Structure {
+	public int lCodebase;
+	public int lMajor;
+	public int lMinor;
+	public int lBuild;
+	public String Version;
+	public String BuildDate;
+	public String CopyRight;
+
+	@Override
+	protected List<String> getFieldOrder() {
+		List<String> list = new ArrayList<String>();
+		list.add("lCodebase");
+		list.add("lMajor");
+		list.add("lMinor");
+		list.add("lBuild");
+		list.add("Version");
+		list.add("BuildDate");
+		list.add("CopyRight");
+		return list;
+	}
+
+	public int getlCodebase() {
+		return lCodebase;
+	}
+
+	public void setlCodebase(int lCodebase) {
+		this.lCodebase = lCodebase;
+	}
+
+	public int getlMajor() {
+		return lMajor;
+	}
+
+	public void setlMajor(int lMajor) {
+		this.lMajor = lMajor;
+	}
+
+	public int getlMinor() {
+		return lMinor;
+	}
+
+	public void setlMinor(int lMinor) {
+		this.lMinor = lMinor;
+	}
+
+	public int getlBuild() {
+		return lBuild;
+	}
+
+	public void setlBuild(int lBuild) {
+		this.lBuild = lBuild;
+	}
+
+	public String getVersion() {
+		return Version;
+	}
+
+	public void setVersion(String version) {
+		Version = version;
+	}
+
+	public String getBuildDate() {
+		return BuildDate;
+	}
+
+	public void setBuildDate(String buildDate) {
+		BuildDate = buildDate;
+	}
+
+	public String getCopyRight() {
+		return CopyRight;
+	}
+
+	public void setCopyRight(String copyRight) {
+		CopyRight = copyRight;
+	}
+
+}
