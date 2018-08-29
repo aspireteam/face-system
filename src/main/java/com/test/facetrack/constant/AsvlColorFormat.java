6717cfe0ad069ff4de4eddf608d605d29f5df6b3
a324670547 <324670547@qq.com>
2018-08-29 18:06:07 +0800

    颜色格式及其对齐规则

---
+++ src/main/java/com/test/facetrack/constant/AsvlColorFormat.java
@@ -0,0 +1,11 @@
+package com.test.facetrack.constant;
+/**
+ * 颜色格式及其对齐规则
+ */
+public class AsvlColorFormat {
+    public static final int ASVL_PAF_I420 = 0x601;
+    public static final int ASVL_PAF_NV12 = 0x801;
+    public static final int ASVL_PAF_NV21 = 0x802;
+    public static final int ASVL_PAF_YUYV = 0x501;
+    public static final int ASVL_PAF_RGB24_B8G8R8 = 0x201;
+}
