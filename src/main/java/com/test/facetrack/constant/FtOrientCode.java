c2faee0cd46010d5238c56ed2f00a321dedaa0db
a324670547 <324670547@qq.com>
2018-08-29 18:10:15 +0800

    定义检测返回的人脸角度

---
+++ src/main/java/com/test/facetrack/constant/FtOrientCode.java
@@ -0,0 +1,14 @@
+package com.test.facetrack.constant;
+/**
+ *  定义检测返回的人脸角度
+ */
+public class FtOrientCode {
+	// 0°
+	public static final int AFT_FSDK_FOC_0 = 0x1;
+	// 90°
+	public static final int AFT_FSDK_FOC_90 = 0x2;
+	// 270°
+	public static final int AFT_FSDK_FOC_270 = 0x3; 
+	// 180°
+	public static final int AFT_FSDK_FOC_180 = 0x4; 
+}
