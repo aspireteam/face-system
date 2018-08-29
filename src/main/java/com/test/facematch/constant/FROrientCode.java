33f3d860af31e293faa20192e139451db971606d
a324670547 <324670547@qq.com>
2018-08-29 18:21:07 +0800

    定义人脸检测结果中的人脸角度

---
+++ src/main/java/com/test/facematch/constant/FROrientCode.java
@@ -0,0 +1,30 @@
+package com.test.facematch.constant;
+/**
+ * 定义人脸检测结果中的人脸角度
+ */
+public class FROrientCode {
+	// 0°
+    public static final int AFD_FSDK_FOC_0 = 0x1;
+    // 90°
+    public static final int AFD_FSDK_FOC_90 = 0x2; 
+    // 270°
+    public static final int AFD_FSDK_FOC_270 = 0x3;
+    // 180°
+    public static final int AFD_FSDK_FOC_180 = 0x4;
+    // 30°
+    public static final int AFD_FSDK_FOC_30 = 0x5; 
+    // 60°
+    public static final int AFD_FSDK_FOC_60 = 0x6; 
+    // 120°
+    public static final int AFD_FSDK_FOC_120 = 0x7; 
+    // 150°
+    public static final int AFD_FSDK_FOC_150 = 0x8; 
+    // 210°
+    public static final int AFD_FSDK_FOC_210 = 0x9;
+    // 240°
+    public static final int AFD_FSDK_FOC_240 = 0xa; 
+    // 300°
+    public static final int AFD_FSDK_FOC_300 = 0xb; 
+    // 330°
+    public static final int AFD_FSDK_FOC_330 = 0xc;
+}
