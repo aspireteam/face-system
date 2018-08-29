fa7bf7127a1a24ba749164e26201ea2da6329c5b
a324670547 <324670547@qq.com>
2018-08-29 18:07:14 +0800

    检测到的脸部信息

---
+++ src/main/java/com/test/facetrack/entity/Faceres.java
@@ -0,0 +1,40 @@
+package com.test.facetrack.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.sun.jna.Pointer;
+import com.sun.jna.Structure;
+import com.sun.jna.ptr.IntByReference;
+import com.test.facetrack.entity.MRect;
+/**
+ *  检测到的脸部信息
+ */
+public class Faceres extends Structure{
+    public int nFace;
+    public MRect.ByReference rcFace;
+    public IntByReference lfaceOrient;
+
+    public Faceres() {}
+    public Faceres(Pointer p) {
+        super(p);
+        read();
+    }
+    
+    public static class ByReference extends Faceres implements Structure.ByReference {
+        public ByReference() {
+
+        }
+        public ByReference(Pointer p) {
+            super(p);
+        }
+    };
+
+    @Override
+    protected List<String> getFieldOrder() {
+    	List<String> list = new ArrayList<String>();
+    	list.add("nFace");
+    	list.add("rcFace");
+    	list.add("lfaceOrient");
+        return list;
+    }
+}
